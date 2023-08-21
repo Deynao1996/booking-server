89cb1b7e9a42aa79ae090d55037a0a3e7e4e6e97
Dmytro <Deynao1996@gmail.com>
2023-08-21 08:01:20 +0300

    whiteList added

--- index.js
+++ index.js
@@ -39,13 +39,7 @@ const whitelist = [process.env.CLIENT_URL, process.env.DASHBOARD_URL]
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
+    origin: whitelist,
     methods: 'GET, POST, DELETE, PUT',
     credentials: true
   })
